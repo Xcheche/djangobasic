72470c8a28b1d95ef356d258137d78049d9ff998
xcheche <chekwubechukwu23@gmail.com>
2024-03-01 19:46:33 +0100

    django-employee-app-basics

---
+++ mysite/views.py
@@ -0,0 +1,15 @@
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+from employees.models import Employees
+
+
+def home(request):
+    employees = Employees.objects.all()
+    context = {'employees': employees,
+
+               }
+    return render(request, 'home.html', context)
